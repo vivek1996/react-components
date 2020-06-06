1d8ceb929a1ebdf194bdcbab4ed13bb4ee785ea2
DEEPAK BANSAL <bansaldeepak1986@gmail.com>
2020-06-06 16:49:28 +0530

    Syntax fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { default as FormField } from "./FormField";
 export { default as Form } from "./Form";
-export { default as Table } from "./Table/Table";
+export { default as Table } from "./Table";
+export { default as FormField } from "./FormField";
