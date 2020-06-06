59f1a4f4216940fbdf0c6462171bd2b8b1594b3d
DEEPAK BANSAL <bansaldeepak1986@gmail.com>
2020-06-06 16:50:15 +0530

    reoved commented code

--- src/Field/index.js
+++ src/Field/index.js
@@ -1,17 +1,1 @@
-// export { default as Input } from './Input';
-// export * from './Input';
-
-// export { default as Range } from './Range';
-// export { default as Select } from './Select';
-// export { default as Toggle } from './Toggle';
-// export { default as Number } from './Number';
-// export { default as Radio } from './Radio';
-// export { default as Checkbox } from './Checkbox';
-// export { default as File } from './File';
-
-// export { default as Autocomplete } from './Autocomplete';
-// export { default as Date } from './Date';
-// export { default as Switch } from './Switch';
-
-// export * from './Field';
 export { default } from "./Field";
